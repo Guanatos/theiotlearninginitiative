ad455fa8d60db33d8dcf7f05742f8d0d9300298c
Daniel Nuno <daniel.nuno@gmail.com>
2016-07-12 23:06:21 +0000

    mqtt

---
+++ InternetOfThings101/hello.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+my_str = "Hello World!!!"
+split = my_str.split()
+print(split)
+A = list(range(10))
+print(A)
+B = [11,12,13,14]
+print(B)
+A.append(12)
+print("Append to A",A)
+age = 55
+if age == 35:
+   print("Age is equal to 35!")
+for x in range(3,16):
+   if x % 2 == 0:
+      print(x)
